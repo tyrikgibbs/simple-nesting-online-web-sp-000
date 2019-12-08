e17b6135fdaf4ec1891c63914d9ae5e39a2d165a
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-08 06:29:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,9 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
+# }]
 
-puts programmer_hash[:yukihiro_matsumoto]
+puts programmer_hash[:red_hopper]
 
 end
 
