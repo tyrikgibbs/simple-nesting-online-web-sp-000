bebe8f08c0b986df590c922d3bc4b5dd8aff2e02
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-08 06:39:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,7 @@ programmer_hash[:yukihiro_matsumoto]
 programmer_hash[:yukihiro_matsumoto][:known_for]
 programmer_hash[:yukihiro_matsumoto][:known_for] << "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages]
-programmer_hash[:yukihiro_matsumoto][:languages] << "LISP", "C"
+programmer_hash[:yukihiro_matsumoto][:languages] << "LISP" + "C"
 
 puts programmer_hash[:yukihiro_matsumoto]
 
