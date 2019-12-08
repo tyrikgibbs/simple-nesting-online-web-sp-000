7e5cfff137d889bdb56f1a33832c433eef510b37
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-08 06:16:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
 dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
-dennis_ritchies_language = dennis_ritchies_language.to_s
+
 end
 
 def adding_matz
